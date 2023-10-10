d4caffa36ae81e15c10fd6992d7ecace9d707466
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2023-10-10 15:31:44 -0700

    added more skeleton files

--- config.js
+++ config.js
@@ -7,8 +7,22 @@ require("dotenv").config;
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
 
-
 const BUCKET = "sharebnb-jm";
-
+const PORT = 3001;
 
 AWS.config.update({ region: 'us-west-1' });
+
+
+/** Use dev database, testing database, or via env var, production database */
+function getDatabaseUri() {
+  return (process.env.NODE_ENV === "test")
+    ? "postgresql:///jobly_test"
+    : process.env.DATABASE_URL || "postgresql:///jobly";
+}
+
+
+module.exports = {
+  AWS,
+  s3,
+  getDatabaseUri,
+};
