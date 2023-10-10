013775d22f81005a913bb12ef243eb126f82ee9e
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2023-10-10 12:30:58 -0700

    created more skeleton files

--- config.js
+++ config.js
@@ -1,1 +1,14 @@
 "use strict";
+
+/** Config options for entire application. */
+
+require("dotenv").config;
+
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+
+
+const BUCKET = "sharebnb-jm";
+
+
+AWS.config.update({ region: 'us-west-1' });
---
+++ models/listing.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const {
+  NotFoundError,
+  BadRequestError,
+  UnauthorizedError,
+} = require("../expressError");
+
+class Listing {
+
+  static async upload(file) {
+    //continue here
+  }
+}
