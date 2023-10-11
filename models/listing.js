1d45fa25e1fc795d444ecce107d0576ddd218ca4
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2023-10-11 16:15:25 -0700

    started on listing model

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const db = require('../db');
 const {
   NotFoundError,
   BadRequestError,
@@ -8,7 +9,35 @@ const {
 
 class Listing {
 
-  static async upload(file) {
-    //continue here
+  /** Adds listing to database with provided data.
+   *
+   * Return => { title, description, price, availability, photos }
+   *    where photos => {...}
+   *
+   */
+  static async addListing({ title, description, price, availability, photos }) {
+    const result = await db.query(`
+        INSERT INTO listings
+          (title,
+           description,
+           price,
+           availability,
+           photos)
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING
+           username,
+           description,
+           price,
+           availability,
+           photos`,
+      [title, description, price, availability, photos]
+    );
+
+    const listing = result.rows[0];
+
+    return listing;
   }
+
+  //TODO: function to convert photo file into url to save in database
+
 }
